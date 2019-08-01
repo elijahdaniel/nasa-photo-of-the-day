a91ec38dd4c753d931402fb9bf7165ae36ab8d39
Elijah Paminsan <e.paminsan@gmail.com>
2019-08-01 08:42:37 -0700

    Converted CSS into styled components

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import Img from "./components/img";
 import Title from "./components/title";
 import "./App.css";
 import axios from "axios";
+import {
+  Header,
+  Bottom,
+  BottomL,
+  BottomR,
+  AlignText,
+  Center
+} from "./components/styles";
 
 function App() {
   const [data, setData] = useState("");
 
   useEffect(() => {
     axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=INWhlK0FxCxJ92WchVebMAdeGZxchjhIvmR31LLu")
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=INWhlK0FxCxJ92WchVebMAdeGZxchjhIvmR31LLu"
+      )
       .then(info => {
         setData(info.data);
       })
@@ -22,17 +32,30 @@ function App() {
 
   return (
     <div className="App">
-      <h1>NASA's Astronomy Picture of the Day</h1>
-      <div className="bottom">
-        <section className="bottomLeft">
-          <Img url={data.hdurl} />
-        </section>
-        <section className="bottomRight">
-          <Title title={data.title} />
-          <Copyright copy={data.copyright} />
-          <Explanation explain={data.explanation} />
-        </section>
-      </div>
+      <Header>
+        <h1>NASA's Astronomy Picture of the Day</h1>
+      </Header>
+      <Bottom>
+        <div className="bottom">
+          <BottomL>
+            <section className="bottomLeft">
+              <Img url={data.hdurl} />
+            </section>
+          </BottomL>
+          <BottomR>
+            <section className="bottomRight">
+              <Center>
+                <Title title={data.title} />
+              
+              <Copyright copy={data.copyright} />
+              </Center>
+              <AlignText>
+                <Explanation explain={data.explanation} />
+              </AlignText>
+            </section>
+          </BottomR>
+        </div>
+      </Bottom>
     </div>
   );
 }
---
+++ src/components/styles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const Header = styled.h1`
+  text-align: center;
+  color: white;
+  margin: 65px;
+`;
+
+export const Bottom = styled.div`
+  color: white;
+  margin: 0 auto;
+  box-shadow: 0px 0px 15px 10px #015;
+`;
+
+export const BottomL = styled.div`
+  width: 50%;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+export const BottomR = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  padding: 50px;
+  box-sizing: border-box;
+  background: #04052e;
+`;
+
+export const AlignText = styled.p`
+  text-align: justify;
+  padding-top: 5px;
+  line-height: 35px;
+`;
+
+export const Center = styled.h2`
+  text-align: center;
+`;
